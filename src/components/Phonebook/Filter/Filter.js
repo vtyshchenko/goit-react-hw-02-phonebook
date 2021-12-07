35aa90fcfe26d55134c3d62470b08b7793861bf0
Volodymyr Tyshchenko <volodymyr.tyshchenko@gmail.com>
2021-12-07 11:22:05 +0200

    bugfix

--- src/components/Phonebook/Filter/Filter.js
+++ src/components/Phonebook/Filter/Filter.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 import styles from './Filter.module.scss';
 
-function Filter({ onChange }) {
+function Filter({ onChange, filter }) {
   return (
     <>
       <label className={styles.label}>
@@ -12,7 +12,7 @@ function Filter({ onChange }) {
           type="text"
           name="name"
           placeholder="John Jonson"
-          value=""
+          value={filter}
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
